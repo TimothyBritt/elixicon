be128f35eb775b722b2cadd0e8257a0f14283bad
Timothy Britt <tim@timothybritt.com>
2017-08-07 23:33:23 -0500

    Elixicon generation and file save working

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,4 +1,5 @@
 defmodule Identicon do
+  require Integer
   @moduledoc """
     Generates identicon avatars from custom strings.
   """
@@ -8,6 +9,10 @@ defmodule Identicon do
     |> Identicon.generate_image
     |> Identicon.pick_color
     |> Identicon.build_grid
+    |> Identicon.filter_odd_cells
+    |> Identicon.build_pixel_map
+    |> Identicon.draw_image
+    |> Identicon.save_image(string)
   end
 
   @doc """
@@ -45,6 +50,10 @@ defmodule Identicon do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
+  def mirror_row([a, b, c]) do
+    [a, b, c, b, a]
+  end
+
   def build_grid(%Identicon.Image{hex: hex_list} = image) do
     grid =
       hex_list
@@ -55,7 +64,39 @@ defmodule Identicon do
       %Identicon.Image{image | grid: grid}
   end
 
-  def mirror_row([a, b, c]) do
-    [a, b, c, b, a]
+  def filter_odd_cells(%Identicon.Image{grid: grid} = image) do
+    filtered =
+      grid
+      |> Enum.filter(fn({value, _}) -> Integer.is_even(value) end )
+
+    %Identicon.Image{image | grid: filtered}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_value, index}) ->
+      top_x = rem(index, 5) * 50
+      top_y = div(index, 5) * 50
+      top_left = {top_x, top_y}
+      bottom_right = {top_x + 50, top_y + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def save_image(image, filename) do
+    File.write("./elixicons/#{filename}.png", image)
   end
 end
--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule Identicon.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:egd, github: "erlang/egd"}
     ]
   end
 end
